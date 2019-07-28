ef03ae94de89ac5e71a5c69f4fd07b534db86ec1
David Little <littledavid175@gmail.com>
2019-07-28 22:06:18 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -58,6 +58,7 @@ def self.create_from_filename(file_name)
    song_artist = file_name.split(" - ")
    song.name = song_artist[1].gsub(".mp3", "")
    song.artist_name = song_artist[0]
+   song
 end
 
 
