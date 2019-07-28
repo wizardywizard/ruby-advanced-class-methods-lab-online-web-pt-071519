a7c31e7fd7d832890402ade556931f0e28bf7614
David Little <littledavid175@gmail.com>
2019-07-28 22:09:32 +0000

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -54,10 +54,13 @@ def self.new_from_filename(file_name)
 end
 
 def self.create_from_filename(file_name)
-  self.new_from_filename(file_name)
+   song = self.new_from_filename(file_name)
+   song.save
 end
 
-
+def self.destroy_all
+  @@all.clear
+end 
 
 
 
